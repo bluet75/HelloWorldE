f067e45ecafd30c000203f606ac4e94be1877f99
bluet75 <bluet75@naver.com>
2021-07-23 07:49:46 +0900

    added 		System.out.println("Add Java Class File Push2");

--- src/PushPrint.java
+++ src/PushPrint.java
@@ -4,7 +4,8 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class File Push");
-		System.out.println("Add Java Class File Push2");		
+		System.out.println("Add Java Class File Push2");
+		System.out.println("Add Java Class File Push3");			
 	}
 
 }
